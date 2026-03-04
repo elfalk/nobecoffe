c81303603c25d13d1dc5a8b619ba8dba7381e235
Yuossef elbadry <yuoseef01102@gmail.com>
2026-03-04 02:20:20 +0200

    Update prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -1,13 +1,9 @@
-// prisma.config.js
-require("dotenv").config();
-const { defineConfig } = require("prisma/config");
+import { PrismaClient } from "@prisma/client";
 
-module.exports = defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-});
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
