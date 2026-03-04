58cb236d2ff7ef3968343018ea989b017d4050c2
Yuossef elbadry <yuoseef01102@gmail.com>
2026-03-04 02:23:58 +0200

    Refactor prisma.config.js to use CommonJS syntax

--- prisma.config.js
+++ prisma.config.js
@@ -1,9 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+// prisma.config.js
+require("dotenv").config();
+const { defineConfig } = require("prisma/config");
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+module.exports = defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
